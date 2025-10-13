de5548706aa3a664f416ba12a9b03b4f0df05302
danny <danny21316@gmail.com>
2025-10-13 11:01:00 +0800

    series web & rwd done

--- HsiehYuanTing/shoes/js/productList.js
+++ HsiehYuanTing/shoes/js/productList.js
@@ -1,11 +1,15 @@
 const list = document.querySelector('.hot-items');
+const series = document.querySelector('.series-items');
 let items = null;
 let str = '';
+let seriesStr = ''
 fetch('../data/products.json').then(res => res.json())
     .then(data => {
         items = [...data];
         combineCard(items);
+        combineSeriesCard(items);
         list.innerHTML = str;
+        series.innerHTML = seriesStr;
     })
 
 function combineCard(data) {
@@ -17,6 +21,21 @@ function combineCard(data) {
                 <div class="hot-tag">
                     <p  class="hot-tag-text">人氣 No.1</p>
                 </div>
+            </div>
+        `
+    }
+}
+
+function combineSeriesCard(data) {
+    for (let i = 0; i < 4; i++) {
+        seriesStr +=
+            `
+            <div class="series-item">
+                    <img src="${data[i].pic}.png" alt="product-card">
+                    <div class="series-tag">
+                        <p class="series-tag-text">${data[i].series}</p>
+                        <p class="series-tag-text">${data[i].seriesCh}</p>
+                    </div>
             </div>
         `
     }
