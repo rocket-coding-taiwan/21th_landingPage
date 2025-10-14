243a4943cfdb858d2488d8fa9234eeb6e9152a3d
Danny-1211 <danny21316@gmail.com>
2025-10-14 18:10:50 +0800

    pagination & productList done

--- HsiehYuanTing/shoes/js/productList.js
+++ HsiehYuanTing/shoes/js/productList.js
@@ -3,34 +3,94 @@
 // 商品頁切換分頁
 
 let items = null;
-let totalPageDom = document.querySelector('.total-page');
+
+const totalPageDom = document.querySelector('.total-page');
+const cardsDom = document.querySelector('.cards');
+
+const maxCount = 9 // 一頁 9 個 item
+
 fetch('../data/products.json').then(res => res.json())
     .then(data => {
         items = [...data];
-        totalPageDom.innerHTML = renderTotalPages(items.length);
+        pagination(items, 1);
     }).catch(err => console.error('資料載入失敗:', err));
 
 
-function renderTotalPages(totalCount) { // 先算總共有幾頁並渲染
-    let maxCount = 9; // 一頁 9 個 item
-    let str = '';
-    let totalPage = totalCount / maxCount;
-    if (totalCount % maxCount !== 0) { // 如果有多的要放到下一頁
-        totalPage += 1;
-    }
+function pagination(data, currentPage) {
+    const totalCount = data.length;
+    totalPageDom.innerHTML = renderPages(totalCount, currentPage);
+    cardsDom.innerHTML = renderCards(data, currentPage);
+}
 
-    for (let i = 1; i <= totalPage; i++) {
+//渲染分頁
+function renderPages(totalCount, currentPage) {
+    let str = '';
+    let totalPage = Math.ceil(totalCount / maxCount);
+    // 當前頁的前兩頁跟後兩頁 跟最後一頁才會被印出來 不然就是被加上一個 ...
+    if (currentPage > totalPage) {
+        currentPage = pageTotal;
+    }
+    pageSet = [
+        1,
+        currentPage - 2,
+        currentPage - 1,
+        currentPage,
+        currentPage + 1,
+        currentPage + 2,
+        totalPage
+    ];
+
+    pageSet = pageSet.filter(item => {
+        return item > 0 && item <= totalPage
+    });
+
+    // 移除重複的頁碼 
+    pageSet = pageSet.filter((item, index) => {
+        return pageSet.indexOf(item) === index;
+    });
+
+    // 從小排到大
+    pageSet.sort((a, b) => a - b);
+
+    pageSet.forEach((i, index) => {
+        if (index > 0 && i - pageSet[index - 1] > 1) {
+            str += `
+            <div class="single-page" >
+                <p>...</p>
+            </div>
+            `
+        }
         str += `
-        <div class="single-page">
-            <p>${i}</p>
+            <div class="single-page" id="${i}">
+                <p>${i}</p>
+            </div>
+            `
+    });
+    return str;
+}
+
+// 渲染商品列表
+function renderCards(data, page) {
+    let str = ''
+    let start = (page - 1) * maxCount;
+    let end = Math.min(page * maxCount, data.length); // 到最後一頁的時候避免超過原本資料的長度
+    for (let i = start; i < end; i++) {
+        str += `
+        <div class="card">
+            <img src="${data[i].pic}.png" alt="${data[i].product_name}">
+            <p class="card-title">${data[i].product_name}</p>
+            <p class="card-price">NT$${data[i].price}</p>
         </div>
         `
     }
-
-    return str
+    return str;
 }
 
 
-function renderCards(data, page = 1) {
-
+function switchPage(e) {
+    e.preventDefault();
+    let currentPage = e.target.closest('.single-page').id;
+    pagination(items, parseInt(currentPage));
 }
+
+totalPageDom.addEventListener('click', switchPage);
