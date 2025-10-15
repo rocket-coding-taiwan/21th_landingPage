226f07f6582f00a91aca8166ea006a30b5e1684d
danny <danny21316@gmail.com>
2025-10-15 13:44:49 +0800

    選擇系列完成

--- HsiehYuanTing/shoes/js/productList.js
+++ HsiehYuanTing/shoes/js/productList.js
@@ -1,38 +1,62 @@
-
-let items = null;
+let items = null; // 原始資料，永不改動
+let filteredItems = null;  // 過濾後的資料，用來渲染
 const totalPageDom = document.querySelector('.total-page');
 const cardsDom = document.querySelector('.cards');
 const preDom = document.querySelector('.switch-pre-tab');
 const nextDom = document.querySelector('.switch-next-tab');
+const selectDom = document.querySelector('.select-ul');
 
 const maxCount = 9; // 一頁 9 個 item
-let totalPageGlobal = 0;
+let totalSeriesGlobal = []; // 所有系列
+let totalPageGlobal = 0; // 總頁數
 let currentPageGlobal = 1; // 紀錄當前頁數
-
+let currentSeriesGlobal = '所有商品'; // 記錄當前選擇的系列 預設是所有商品
 fetch('../data/products.json').then(res => res.json())
     .then(data => {
         items = [...data];
-        pagination(items, currentPageGlobal);
-        updatePaginationBtn();
+        filteredItems = [...data];
+        totalSeriesGlobal = selectSeries(items);
+        pagination(filteredItems, currentPageGlobal);
     }).catch(err => console.error('資料載入失敗:', err));
-
 
 function pagination(data, currentPage) {
     const totalCount = data.length;
     totalPageGlobal = Math.ceil(totalCount / maxCount);
-    totalPageDom.innerHTML = renderPages(totalCount, currentPage);
+    totalPageDom.innerHTML = renderPages(currentPage);
     cardsDom.innerHTML = renderCards(data, currentPage);
+    selectDom.innerHTML = renderSeries();
     updatePaginationBtn();
 }
-
-//渲染分頁
-function renderPages(totalCount, currentPage) {
+// 抓出所有的系列
+function selectSeries(data) {
+    let series = data.map(item => item.seriesCh);
+    series = series.filter((item, index) => {
+        return series.indexOf(item) == index;
+    })
+    series.unshift('所有商品');
+    return series;
+}
+// 依照系列過濾資料 
+function filterBySeries(seriesName) {
+    currentPageGlobal = 1;
+    currentSeriesGlobal = seriesName;
+    if (seriesName === '所有商品') {
+        filteredItems = [...items];  // 復原全部
+    } else {
+        filteredItems = items.filter(item => {
+            return item.seriesCh == seriesName;
+        })
+    }
+    pagination(filteredItems, currentPageGlobal);
+}
+//  渲染分頁
+function renderPages(currentPage) {
     let str = '';
     // 當前頁的前兩頁跟後兩頁 跟最後一頁才會被印出來 不然就是被加上一個 ...
     if (currentPage > totalPageGlobal) {
         currentPage = totalPageGlobal;
     }
-    pageSet = [
+    let pageSet = [
         1,
         currentPage - 2,
         currentPage - 1,
@@ -70,8 +94,7 @@ function renderPages(totalCount, currentPage) {
     });
     return str;
 }
-
-// 渲染商品列表
+//  渲染商品列表
 function renderCards(data, page) {
     let str = ''
     let start = (page - 1) * maxCount;
@@ -87,27 +110,47 @@ function renderCards(data, page) {
     }
     return str;
 }
-
-
+// 渲染系列選擇器
+function renderSeries() {
+    let str = '';
+    totalSeriesGlobal.forEach((item, index) => {
+        str += `
+            <li class="${item == currentSeriesGlobal ? 'active' : ''}" id=${index} data-series="${item}">${item}</li>
+        `
+    })
+    return str;
+}
+//  直接選選擇哪一頁
 function switchPage(e) {
     currentPageGlobal = parseInt(e.target.closest('.single-page').id);
-    pagination(items, currentPageGlobal);
+    if (currentPageGlobal) { // 如果點到 ... 就不執行切分頁
+        pagination(filteredItems, currentPageGlobal);
+    }
 }
-
-function prePage(e) {
+//  上一頁
+function prePage() {
     if (currentPageGlobal > 1) {
         currentPageGlobal -= 1;
-        pagination(items, currentPageGlobal);
+        pagination(filteredItems, currentPageGlobal);
     }
 }
-
-function nextPage(e) {
+//  下一頁
+function nextPage() {
     if (currentPageGlobal < totalPageGlobal) {
         currentPageGlobal += 1;
-        pagination(items, currentPageGlobal);
+        pagination(filteredItems, currentPageGlobal);
+    }
+}
+// 抓點擊現在的系列
+function takeSelect(e) {
+    let selectedLi = e.target.closest('li');
+    if (selectedLi) {
+        let seriesName = selectedLi.getAttribute('data-series');
+        filterBySeries(seriesName);
     }
 }
 
+// 分頁按鈕 diabled enabled
 function updatePaginationBtn() {
     // 上一頁按鈕
     if (currentPageGlobal === 1) {
@@ -131,3 +174,4 @@ function updatePaginationBtn() {
 totalPageDom.addEventListener('click', switchPage);
 preDom.addEventListener('click', prePage);
 nextDom.addEventListener('click', nextPage);
+selectDom.addEventListener('click', takeSelect);
